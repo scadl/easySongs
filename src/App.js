fcfb5bd3292cad1b3bdffc61354e32205b6f4d66
Alexandr <scad.luncher@gmail.com>
2020-06-19 18:57:12 +0300

    prefixes & remove

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 //import ReactDOM from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import {
-  Button, Intent, ButtonGroup, AnchorButton, Navbar,
-  Alignment, Card, Elevation, Position, Tooltip, InputGroup, Label
+  Button, Intent, ButtonGroup, Navbar,
+  Alignment, Card, Elevation, Position, Tooltip, InputGroup, Label, NumericInput
 } from "@blueprintjs/core"
 
 
@@ -34,9 +33,18 @@ class NoteChar extends React.Component{
       ["ю","ж","з","0","(","З","Ж","o","V","=","=","[","\\","/"],
       [".","э","х","-","=","Х","Э","p","B","=","=","[","\\","/"],
       ["Ю",",","ъ","+","=","=","=","=","N","=","=","[","\\","/"],
+    ];
+    let slavToArab = [
+      "а7","в7","г7","д7","є7","ѕ7","з7","и7"
     ]
+    let noteSourcesUniversal = [
+      "=","[","\\","/"," ",      
+      <span>
+      <span className="glas_nm">{this.props.pristN} {slavToArab[this.props.glasN-1]} </span>
+      [-</span>
+    ];
     let stepInc = 4;
-    let stepPos = 45;
+    let stepPos = 50;
     let noteHights = [];
     noteSources.forEach((note)=>{      
       noteHights.push([ stepPos, stepPos-stepInc, note ]);
@@ -52,21 +60,26 @@ class NoteChar extends React.Component{
               return false;
             }
         });
-        //console.log(nCh);
-        if(nCh===undefined || this.props.CnID === -1){
-          return {cNote: "="}
+        console.log(nCh);
+        if(nCh===undefined || this.props.CnID === -1){      
+          return {cNote: "="};          
         } else {
-          return {cNote: nCh[2][this.props.CnID]}
+          if(this.props.CnID > 9){
+            return {cNote: noteSourcesUniversal[this.props.CnID-10]}
+          } else {
+            return {cNote: nCh[2][this.props.CnID]}
+          }
         }
       }
-    })
-    //console.log(" cY:"+this.props.cy +  " eCy:" + evt.clientY + " rY:"+clickY);
+    });
+    this.props.onNoteUpdate();
+    console.log(" cY:"+this.props.cy +  " eCy:" + evt.clientY + " rY:"+clickY);
   }
-  render(){
+  render(){    
     return(
-      <span onClick={this.noteUpdate}>
+      <div onClick={this.noteUpdate} style={{display:"inline"}}>
       {this.state.cNote}
-      </span>
+      </div>
     );
   }
 }
@@ -74,22 +87,33 @@ class NoteChar extends React.Component{
 class NoteLine extends React.Component{
   constructor(props){
     super(props);
+    this.updateCoord = this.updateCoord.bind(this);
     this.state = {
       cx: 0,
-      cy: 0
+      cy: 0,
+      cL: null
     }
   }
   componentDidMount(){
-        this.setState({
-          cx: this.containerLine.offsetLeft,
-          cy: this.containerLine.offsetTop
-        });        
+    this.setState({
+      cL: this.containerLine
+    });  
+    this.updateCoord();
+  }
+  updateCoord(){
+    if (this.state.cL!==null) {
+      this.setState((state) => ({
+        cx: state.cL.offsetLeft,
+        cy: state.cL.offsetTop
+      }));
+    }
   }
   render(){
-    let myLine = "[-`===========================================================\\/";
+    let myLine = "=`===============================================================";
     let resLine = myLine.split("").map((char, key) => 
-      <NoteChar note={char} key={key} 
-      cx={this.state.cx} cy={this.state.cy} CnID={this.props.SnID} />      
+      <NoteChar note={char} key={key} onNoteUpdate={this.updateCoord}
+      cx={this.state.cx} cy={this.state.cy} CnID={this.props.SnID} 
+      glasN={this.props.glas} pristN={this.props.prist} />      
     );
     //console.log(this.props)
     return(
@@ -177,7 +201,7 @@ class TextElement extends React.Component{
     let myTExt = event.target.value;
     //console.log(myTExt)
     this.setState((state)=>({
-      theVal: myTExt
+      theVal: myTExt===""?"§":myTExt
     }));
   }
   sentData(){        
@@ -192,7 +216,7 @@ class TextElement extends React.Component{
         <Button icon={"tick"} minimal={true} onClick={this.sentData}/>
       } />;
     } else {
-      elem = <Label onClick={this.sentData}> 
+      elem = <Label onClick={this.sentData} style={{cursor:"text"}}> 
       {this.state.theVal} 
       </Label>
     }
@@ -246,9 +270,15 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.printCall = this.printCall.bind(this);
-    this.onActiveNote = this.onActiveNote.bind(this)
+    this.onActiveNote = this.onActiveNote.bind(this);
+    this.onLineAdd = this.onLineAdd.bind(this);
+    this.onGlasChange = this.onGlasChange.bind(this);
+    this.onPristChange = this.onPristChange.bind(this);
     this.state = {
       noteID: -1,
+      noteLines: 1,
+      glasNm: 1,
+      prist: "Пёснь"
     }
   }
   
@@ -260,6 +290,29 @@ class App extends React.Component {
     this.setState({
       noteID: nID
     })
+  }
+
+  componentDidMount(){
+  }
+
+  onLineAdd(val){
+    //console.log(valueAsNumber+" "+this.state.noteLines.length/2);
+    this.setState({
+      noteLines:val
+    })
+  }
+
+  onGlasChange(val){
+    this.setState({
+      glasNm:val
+    })
+  }
+
+  onPristChange(evt){
+    let itxt = evt.target.value; 
+    this.setState((state)=>({
+      prist: itxt
+    }))
   }
 
   render() {
@@ -279,12 +332,17 @@ class App extends React.Component {
       ["[", "Начало строки", -5],
       ["\\", "Конец строки", -5],
       ["/", "Конец песнопения", -5],
+      ["Σ", "Удалить символ",  -13],
+      ["Τ", "Вставить приставку",  -13], 
     ];
 
-    let noteLines = []
-    for(var i=0; i<5; i++){
-      noteLines.push(<NoteLine key={"l"+i} SnID={this.state.noteID} />)
-      noteLines.push(<NoteText key={"t"+i} txtID={i}/>)
+    let lines = [];
+    for(let i=0; i<this.state.noteLines; i++){
+    lines.push(
+      <NoteLine key={"l"+i} SnID={this.state.noteID} 
+      glas={this.state.glasNm} prist={this.state.prist} />,
+      <NoteText key={"t"+i} txtID={i}/>
+    )
     }
 
     return (
@@ -298,15 +356,28 @@ class App extends React.Component {
               простоТопорики
            </Navbar.Heading>
             <Navbar.Divider />
-            <AnchorButton className="bp3-minimal" icon="folder-open" text="Открыть" />
+            <Button className="bp3-minimal" icon="folder-open" text="Открыть" />
             <Button className="bp3-minimal" icon="floppy-disk" text="Сохранить" />
             <Button className="bp3-minimal" icon="print" text="Печать" onClick={this.printCall} />
+            <Navbar.Divider />
+            <Tooltip content={"Строк на странице"}>
+            <NumericInput leftIcon="sort" min={1} max={10} value={1} 
+            style={{width:70}} onValueChange={this.onLineAdd} />
+            </Tooltip>
+            <Tooltip content={"Приставка"}>
+            <InputGroup leftIcon="sort-alphabetical" value={this.state.prist} 
+            style={{width:100}} onChange={this.onPristChange} />
+            </Tooltip>
+            <Tooltip content={"Номер"}>
+            <NumericInput leftIcon="changes" min={1} max={8} value={1} 
+            style={{width:70}} onValueChange={this.onGlasChange} />
+            </Tooltip>
           </Navbar.Group>
         </Navbar>
         <header className="App-header">
           <Card elevation={Elevation.TWO} className="printCard" id="myPage">
-            <h3>Композиция 1</h3>
-            {noteLines}
+            <h3><NoteText key={"tH"} txtID={"H"}/></h3>
+            {lines}
           </Card>
           <ButtonGroup vertical={true} large={true} style={{ padding: 15 }}>
             <NoteBar symbols={notes} onActiveN={this.onActiveNote} />
